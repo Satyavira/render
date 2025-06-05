7861a3d36747b993c3c762dcc7419877d6104519
Satyavira <viraprathama@gmail.com>
2025-06-06 00:26:05 +0700

    fix: remove end_date and fetch until the latest possible

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,16 +1,14 @@
 import yfinance as yf
 import pandas as pd
-import datetime 
 
 def fetch_close_data():
     tickers = ['BBCA.JK', 'BYAN.JK', 'TPIA.JK', 'BBRI.JK', 'BMRI.JK',
                'DSSA.JK', 'TLKM.JK', 'ASII.JK', 'BBNI.JK', 'ICBP.JK']
 
-    end_date = datetime.datetime.today().strftime('%Y-%m-%d')
     stock_close_datas = {}
 
     for ticker in tickers:
-        df = yf.download(ticker, start='2018-01-01', end=end_date, interval='1d')
+        df = yf.download(ticker, start='2018-01-01', end=None, interval='1d')
         stock_close_datas[ticker] = df['Close']
         
     df_all = pd.concat(stock_close_datas, axis=1)
